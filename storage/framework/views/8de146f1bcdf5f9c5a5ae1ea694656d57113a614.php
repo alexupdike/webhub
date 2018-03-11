3aba905fbeeb8d3eeb4865a5cd143ea282425de3
Alex Updike <staticalex412@aol.com>
2018-03-11 00:29:50 -0600

    Setting up local dev.

--- storage/framework/views/8de146f1bcdf5f9c5a5ae1ea694656d57113a614.php
+++ storage/framework/views/8de146f1bcdf5f9c5a5ae1ea694656d57113a614.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Webhub
                 </div>
 
                 <div class="links">
